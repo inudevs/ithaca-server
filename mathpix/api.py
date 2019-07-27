727605998c72bb2973e947d693f041a3340bb6f6
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-28 03:00:49 +0900

    feat(latex): mathpix api for converting image to math

---
+++ mathpix/api.py
@@ -0,0 +1,38 @@
+import sys
+import base64
+import requests
+import json
+import configparser
+import os.path
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+config = configparser.ConfigParser()
+config.read(os.path.join(__location__, '../config.ini'))
+app_id = config['MATHPIX']['app_id']
+app_key = config['MATHPIX']['app_key']
+
+def encode_image(filepath):
+    with open(filepath, 'rb') as fp:
+        image_uri = 'data:image/jpg;base64,{}'.format(
+            base64.b64encode(fp.read()))
+    return image_uri
+
+def predict(image_uri):
+    r = requests.post('https://api.mathpix.com/v3/latex',
+                      data=json.dumps({
+                          'src': image_uri,
+                          'formats': ['text', 'latex_normal', 'latex_styled']
+                      }),
+                      headers={
+                          'app_id': app_id,
+                          'app_key': app_key,
+                          'Content-type': 'application/json'
+                      })
+    print(json.dumps(json.loads(r.text), indent=4, sort_keys=True))
+
+
+if __name__ == '__main__':
+    image_uri = encode_image('./test.png')
+    predict(image_uri)
