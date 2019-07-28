e0312da28a5df4642bbfaee9dc4598fea6a22475
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-28 14:33:04 +0900

    feat(server): login api

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,14 +1,6 @@
 from sanic import Blueprint
-from sanic.response import json as res_json
-# from server.api.auth import auth_api
-
-test = Blueprint('test', url_prefix='/')
-
-@test.route('/')
-async def bp1_route(request):
-    return res_json({ 'test': True })
+from server.api.auth import auth_api
 
 api = Blueprint.group(
-    test,
-    # auth_api
+    auth_api
 )
---
+++ server/api/auth/models.py
@@ -0,0 +1,22 @@
+from sanic_openapi import doc
+
+class LoginModel:
+    email = doc.String('이메일', required=True)
+    password = doc.String('패스워드', required=True)
+
+class UserModel:
+    id = doc.Integer('사용자 오브젝트 id')
+    name = doc.String('이름')
+    grade = doc.String('학교')
+    grade = doc.Integer('학년')
+    klass = doc.Integer('반')
+    number = doc.Integer('번호')
+    photo = doc.String('프로필 사진')
+    user_type = doc.String('회원 종류')
+    email = doc.String('이메일')
+    password = doc.String('패스워드')
+
+class TokenModel:
+    token = doc.String('JWT 토큰')
+    refresh_token = doc.String()
+    user = UserModel
