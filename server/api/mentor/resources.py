b42f9acfe8d16d4d367ec9a44d7c7738c0375e77
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-31 00:15:42 +0900

    feat(server): mentoring approve api

--- server/api/mentor/resources.py
+++ server/api/mentor/resources.py
@@ -35,8 +35,34 @@ async def MentoringRequest(request, token: Token, question_id):
 
 @mentor_api.post('/approve/<question_id>')
 @doc.summary('멘토링 승인')
-async def MentoringApprove(request, question_id):
-    pass
+async def MentoringApprove(request, token: Token, question_id):
+    user = token.jwt_identity
+
+    # question 쿼리, 작성자 확인
+    question = await request.app.db.requests.find_one({
+        'user_id': user['id'],
+        'question_id': question_id,
+    })
+    if not question:
+        abort(404)
+    
+    # 다른 멘토링이 이미 승인된 것은 아닌지 확인
+    if await request.app.db.requests.find_one({
+        'question_id': question_id,
+        'approved': True
+    }):
+        abort(400)
+
+    # post로 받은 request_id를 가진 request의 approved를 True로 업데이트
+    request_id = request.json['request_id']
+    res = await request.app.db.requests.update_one({'_id': ObjectId(request_id)}, {
+        '$set': {
+            'approved': True
+        }
+    })
+    if not res.acknowledged:
+        abort(500)
+    return res_json({})
 
 
 @mentor_api.get('/')
