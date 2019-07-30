714c4349a8b798ddd7cfabed45f087bddbf6a44b
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-30 16:11:47 +0900

    test(server): test for school autocomplete search

---
+++ test/test_search.py
@@ -0,0 +1,12 @@
+import pytest
+
+async def test_school_autocomplete(test_cli):
+
+    async def check_resp(query, result):
+        resp = await test_cli.get('/search/school/{}'.format(query))
+        assert resp.status == 200
+        resp_json = await resp.json()
+        assert resp_json == result
+    
+    await check_resp('한국디', ['한국디지털미디어고등학교'])
+    await check_resp('정발', ['정발고등학교', '정발중학교', '정발초등학교'])
